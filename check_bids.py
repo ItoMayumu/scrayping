15a99c11be8f1f0f29e6f007697f54bae7668073
mayumu <mayumu329@icloud.com>
2025-03-17 11:25:57 +0900

    change

--- check_bids.py
+++ check_bids.py
@@ -136,7 +136,7 @@ if login_response.ok:
                     print(f"{JSON_FILE} に保存しました")
                     
                     message = f"(test)新しい荷物が追加されました: {new_shipment_ids}"
-                    send_to_slack(message)
+                    # send_to_slack(message)
             else:
                 print("shipmentId が見つかりませんでした")
 
